70c7d95ea01d270ca15558412f9ffb938b17eb98
김준수 <rhdeotod1023@gmail.com>
2021-10-31 11:35:19 +0900

    시작화면 90%

---
+++ Baemin_Dante/Baemin_Dante/BridgeHeader.h
@@ -0,0 +1,14 @@
+//
+//  BridgeHeader.h
+//  Baemin_Dante
+//
+//  Created by 준수김 on 2021/10/31.
+//
+
+#ifndef BridgeHeader_h
+#define BridgeHeader_h
+
+#import <DaumMap/MTMapView.h>
+
+#endif /* BridgeHeader_h */
+
--- Baemin_Dante/Baemin_Dante/Source/Start/nowLocationViewController.swift
+++ Baemin_Dante/Baemin_Dante/Source/Start/nowLocationViewController.swift
@@ -7,18 +7,62 @@
 
 import UIKit
 
-class nowLocationViewController: UIViewController {
+public let DEFAULT_POSITION = MTMapPointGeo(latitude: 36.22473, longitude: 128.31230)
+class nowLocationViewController: UIViewController, MTMapViewDelegate {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
+    var mapView: MTMapView?
+    var mapPoint1: MTMapPoint?
+    var poiItem1: MTMapPOIItem?
     
-
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            
+            mapView = MTMapView(frame: self.view.frame)
+            
+            if let mapView = mapView {
+                mapView.frame = CGRect(x: 0, y: 70, width: 390, height: 570)
+                        mapView.delegate = self
+                        mapView.baseMapType = .standard
+                        
+                        //MARK: -  지도 중심점, 레벨
+                        mapView.setMapCenter(MTMapPoint(geoCoord: DEFAULT_POSITION), zoomLevel: -2, animated: true)
+                        
+                        //MARK: -  현재 위치 트래킹
+                        mapView.showCurrentLocationMarker = true
+                        mapView.currentLocationTrackingMode = .onWithoutHeading
+                        
+                        //MARK: -  마커 추가
+                        self.mapPoint1 = MTMapPoint(geoCoord: MTMapPointGeo(latitude:  36.22473, longitude: 128.31230))
+                        poiItem1 = MTMapPOIItem()
+                        poiItem1?.markerType = MTMapPOIItemMarkerType.redPin
+                        poiItem1?.mapPoint = mapPoint1
+                        poiItem1?.itemName = "움직여 위차를 설정하세요." //위치 마크 이름
+                        mapView.add(poiItem1)
+                        
+            //            mapView.addPOIItems([poiItem1,poiItem2]
+            //            mapView.fitAreaToShowAllPOIItems()
+                        
+                        self.view.addSubview(mapView)
+                    }
+    
+        }
+    
+    //MARK: - 현재 위치 트래킹 함수
+    func mapView(_ mapView: MTMapView!, updateCurrentLocation location: MTMapPoint!, withAccuracy accuracy: MTMapLocationAccuracy) {
+           let currentLocation = location?.mapPointGeo()
+           if let latitude = currentLocation?.latitude, let longitude = currentLocation?.longitude{
+               print("MTMapView updateCurrentLocation (\(latitude),\(longitude)) accuracy (\(accuracy))")
+           }
+       }
+       
+       func mapView(_ mapView: MTMapView?, updateDeviceHeading headingAngle: MTMapRotationAngle) {
+           print("MTMapView updateDeviceHeading (\(headingAngle)) degrees")
+       }
+    
     @IBAction func back(_ sender: UIButton) {
         dismiss(animated: true, completion: nil)
     }
     
 
 }
+
